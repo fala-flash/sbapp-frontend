f04393cc410bf721774ae8735f7625435cd6e20b
fala-flash <andrea0falaschini@gmail.com>
2020-03-13 16:20:44 +0100

    comment

--- src/app/pages/blog/blog/blog.component.ts
+++ src/app/pages/blog/blog/blog.component.ts
@@ -9,22 +9,28 @@ import * as moment from 'moment';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
+
+
+  // dati profilo utente loggato, e che quindi può commentare
   text: string;
   authorID = '';
   authorName = '';
   authorEmail = '';
   authorTel = 0;
   
-  IDPOST = ''
-  posts = []
-  comments: string[] = []
+  // dati relativi ai post
+  IDPOST = ''  //id del post su cui clicco commenta
+  posts = []   // array contenente tutti i post
+  comments: string[] = []  //array che serve per gestire la textarea
+
 
   constructor(private flashMessagesService: FlashMessagesService,
               private authService:AuthService) { }
 
   ngOnInit() {
     this.authService.getPosts().subscribe(post => {
-      this.posts = post;      
+      this.posts = post;
+      this.posts.reverse(); //così in cima c'è sempre l'ultimo inserito     
       this.initComments();
     },
      err => {
@@ -42,6 +48,7 @@ export class BlogComponent implements OnInit {
        console.log(JSON.stringify(err));
        return false;
      });
+     
   }
 
   getPostId(POSTID: any){
@@ -50,6 +57,7 @@ export class BlogComponent implements OnInit {
   }
 
   onCommentSubmit(i: number){
+    
 
     //if a comment is null, it becomes an empty string (if not, can cause problem)
     if (this.comments[i] == null ) {
@@ -57,7 +65,7 @@ export class BlogComponent implements OnInit {
     }
     const comment = {
       text: this.comments[i].replace(/(\r\n|\n|\r)/gm, ""),
-      date: moment().format('DD:MM:YYYY').toString(),
+      date: moment().format('DD/MM/YYYY').toString(),
       time: moment().format('HH:mm:ss').toString(),
       author: this.authorEmail
     }
@@ -71,6 +79,7 @@ export class BlogComponent implements OnInit {
     //post comment
 
     this.authService.addComment(this.IDPOST, comment).subscribe(data => {
+      this.ngOnInit();  //così vedo il commento appena inserito!
       if ((data as any).success) {
         this.flashMessagesService.show('Commento inserito', { cssClass: 'alert-success', timeout: 3000 });
       } else {
@@ -79,16 +88,7 @@ export class BlogComponent implements OnInit {
     })
 
     
-  
-    
-    
-    
-    this.ngOnInit();
 
-
-    /* prima della funzione this.ngOnInit(), inviare il commento al db e creare uno spazio per i commenti
-    facendo così, quando commento e invio il commento al database, il componenente verrà aggiornato
-    nuovamente, caricando il commento appena inserito */
   }
 
   initComments(){
--- src/app/pages/personal-blog/personal-blog/personal-blog.component.ts
+++ src/app/pages/personal-blog/personal-blog/personal-blog.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-personal-blog',
@@ -8,21 +10,40 @@ import { AuthService } from '../../../services/auth.service';
 })
 export class PersonalBlogComponent implements OnInit {
 
-  posts = [];
   userID = '';
 
-  constructor(private authService:AuthService) { }
+  // dati profilo utente loggato, e che quindi può commentare
+  text: string;
+  authorID = '';
+  authorName = '';
+  authorEmail = '';
+  authorTel = 0;
+
+  // dati relativi ai post
+  IDPOST = ''  //id del post su cui clicco commenta
+  posts = []   // array contenente tutti i post
+  comments: string[] = []  //array che serve per gestire la textarea
+
+  
+
+  constructor(private flashMessagesService: FlashMessagesService,private authService:AuthService) { }
 
   ngOnInit() {
     this.authService.getProfile().subscribe(profile => {
       this.userID = profile.user._id;
+      this.authorName = profile.user.name;
+      this.authorEmail = profile.user.email;
+      this.authorTel = profile.user.tel;
       this.authService.getUserPosts(this.userID).subscribe(post => {
-        this.posts = post;  
+        this.posts = post;
+        this.posts.reverse(); //così in cima c'è sempre l'ultimo inserito
+        this.initComments();
       },
        err => {
          console.log(JSON.stringify(err));
          return false;
        });
+       
        
     },
      err => {
@@ -31,6 +52,52 @@ export class PersonalBlogComponent implements OnInit {
      });
 
      
+  }
+
+
+  getPostId(POSTID: any){
+    let postId: string = <unknown>POSTID.innerHTML as string;
+    this.IDPOST = postId;
+  }
+
+  onCommentSubmit(i: number){
+    
+
+    //if a comment is null, it becomes an empty string (if not, can cause problem)
+    if (this.comments[i] == null ) {
+      this.comments[i] = '';
+    }
+    const comment = {
+      text: this.comments[i].replace(/(\r\n|\n|\r)/gm, ""),
+      date: moment().format('DD/MM/YYYY').toString(),
+      time: moment().format('HH:mm:ss').toString(),
+      author: this.authorEmail
+    }
+
+    //validate message
+    if (!comment.text.replace(/\s/g, '').length ) {
+      this.flashMessagesService.show('Scrivi un commento', { cssClass: 'alert-danger', timeout: 3000 });
+      return false;
+    }
+
+    //post comment
+    this.authService.addComment(this.IDPOST, comment).subscribe(data => {
+      this.ngOnInit();  //così vedo il commento appena inserito!
+      if ((data as any).success) {
+        this.flashMessagesService.show('Commento inserito', { cssClass: 'alert-success', timeout: 3000 });
+      } else {
+        this.flashMessagesService.show('Commento non inserito', { cssClass: 'alert-danger', timeout: 3000 });
+      }
+    })
+
+    
+
+  }
+
+  initComments(){
+    for(var i = 0; i<this.posts.length; i++){
+      this.comments.push("");
+    }
   }
 
   
